f3ea6803c5e5ff8aad2e3784035a8d92a999676b
vdng9338 <vdng9338@users.noreply.github.com>
2023-12-29 17:01:10 +0100

    Rename classifier script and make proportion of images assigned to labels configurable

--- classify_images.py
+++ classify_images.py
@@ -9,6 +9,7 @@ import os
 from os.path import isfile
 from tqdm import tqdm
 import json
+import argparse
 
 class EfficientNetB0(nn.Module):
     def __init__(self):
@@ -34,16 +35,23 @@ class EfficientNetB0(nn.Module):
         return x
     
 def main():
-    if len(sys.argv) != 3:
+    parser = argparse.ArgumentParser(
+        prog="classify_images",
+        description="Classify images in a dataset and create two sets of images per attribute in order to apply InterFaceGAN on them.")
+    parser.add_argument("dataset_path")
+    parser.add_argument("output_path")
+    parser.add_argument("-p", "--proportion", help="Proportion of images to put to each group of each attribute", default=0.02)
+    args = parser.parse_args()
+    """if len(sys.argv) != 3:
         print(f"Usage: python3 {sys.argv[0]} <path to folder containing images> <path to output folder>")
-        return 1
+        return 1"""
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     net = EfficientNetB0()
     net.load_state_dict(torch.load('atclas2.pt'))
     net = net.to(device)
     net.eval()
     
-    filenames = [f for f in os.listdir(sys.argv[1]) if isfile(os.path.join(sys.argv[1], f))]
+    filenames = [f for f in os.listdir(args.dataset_path) if isfile(os.path.join(args.dataset_path, f))]
     batch_size = 1
     filenames_and_scores = [[] for _ in range(40)]
     totensor = transforms.ToTensor()
@@ -51,7 +59,7 @@ def main():
         batch = torch.zeros((batch_size, 3, 1024, 1024))
         batch_filenames = filenames[i:min(i+batch_size, len(filenames))]
         for j, f in enumerate(batch_filenames):
-            path = os.path.join(sys.argv[1], f)
+            path = os.path.join(args.dataset_path, f)
             if not isfile(path):
                 continue
             img = Image.open(path)
@@ -64,13 +72,13 @@ def main():
             for att in range(40):
                 filenames_and_scores[att].append((f, out[j, att].item()))
     for att in range(40):
-        with open(os.path.join(sys.argv[2], f"att{att}_scores.json"), "w") as outfile:
+        with open(os.path.join(args.output_path, f"att{att}_scores.json"), "w") as outfile:
             json.dump(dict(filenames_and_scores[att]), outfile)
         filenames_and_scores[att].sort(key=lambda p: p[1])
-        num_top = 100
+        num_top = int(args.proportion*len(filenames))
         filenames_minus1 = [(p[0], -1) for p in filenames_and_scores[att][:num_top]]
         filenames_plus1 = [(p[0], 1) for p in filenames_and_scores[att][-num_top:]]
-        with open(os.path.join(sys.argv[2], f"att{att}_labels.json"), "w") as outfile:
+        with open(os.path.join(args.output_path, f"att{att}_labels.json"), "w") as outfile:
             json.dump(dict(filenames_minus1 + filenames_plus1), outfile)
 
 if __name__ == '__main__':
